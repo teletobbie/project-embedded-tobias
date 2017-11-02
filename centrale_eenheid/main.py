0f24b2ce4b153033077e9d4ad51936d62c476b13
Tobias <tobiasschiphorst@live.nl>
2017-11-02 14:39:30 +0100

    lezen van waarde arduino

--- centrale_eenheid/main.py
+++ centrale_eenheid/main.py
@@ -1,45 +1,16 @@
-#https://www.youtube.com/watch?v=8eM0qpSuIlw
+#https://arduino-er.blogspot.nl/2015/04/python-to-plot-graph-of-serial-data.html
 import serial
 import matplotlib.pyplot as plt
 from drawnow import *
+import atexit
 
-values = []
+serialArduino = serial.Serial('COM5', 9600)
 
-plt.ion()
-cnt = 0
-
-serialArduino = serial.Serial('COM5', 57600)
-
-
-def plotValues():
-    plt.title('Serial value from Arduino')
-    plt.grid(True)
-    plt.ylabel('Values')
-    plt.plot(values, 'rx-', label='values')
-    plt.legend(loc='upper right')
-
-
-# pre-load dummy data
-for i in range(0, 26):
-    values.append(0)
+print("serialArduino.isOpen() = " + str(serialArduino.isOpen()))
 
 while True:
     while (serialArduino.inWaiting() == 0):
         pass
-    valueRead = serialArduino.readline()
-
-    # check if valid value can be casted
-    try:
-        valueInInt = int(valueRead)
-        print(valueInInt)
-        if valueInInt <= 1024:
-            if valueInInt >= 0:
-                values.append(valueInInt)
-                values.pop(0)
-                drawnow(plotValues)
-            else:
-                print("Invalid! negative number")
-        else:
-            print("Invalid! too large")
-    except ValueError:
-        print("Invalid! cannot cast")
+    print("readline()")
+    valueRead = serialArduino.readline(1)
+    print(int(valueRead.hex(), 16))
