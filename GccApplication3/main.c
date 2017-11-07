c6a1bf6a7903fa5cc55bea1dd52c1b9dacc37b5a
Jesse van Veen <perunassi@gmail.com>
2017-11-07 13:27:29 +0100

    Proberen te pushen.

--- GccApplication3/main.c
+++ GccApplication3/main.c
@@ -121,9 +121,10 @@ int main(void)
 		
 		//printf("tempratuur %.1f\n", temperature);
 	
-		int inputLigth = analogRead(1);
-		float voltageLight = inputLigth * 5.0;
+		int inputLight = analogRead(1);
+		float voltageLight = inputLight * 5.0;
 		voltageLight /= 1024;
+		
 		float rldr = (10*voltageLight)/(5-voltageLight);
 		float lux = 500/rldr;
 		
